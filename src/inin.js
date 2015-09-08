f784d5b7d9be7670ff83a9586a397faa2aacceab
Andrew Koroluk <koroluka@gmail.com>
2015-09-08 12:53:50 -0400

    style(inin.js): tabs -> spaces

--- src/inin.js
+++ src/inin.js
@@ -5,27 +5,27 @@ const VERSION = '0.0.1';
 import _ from 'lodash';
 
 const defaults = {
-	key: null,
-	secret: null,
+    key: null,
+    secret: null,
 
-	headers: {
-		'Accept': '*/*',
-		'Connection': 'close',
-		'User-Agent': 'inin-node/' + VERSION
-	},
+    headers: {
+        'Accept': '*/*',
+        'Connection': 'close',
+        'User-Agent': 'inin-node/' + VERSION
+    },
 
-	rest_base: 'https://public-api.us-east-1.inindca.com/api/v1',
+    rest_base: 'https://public-api.us-east-1.inindca.com/api/v1',
 
-	secure: false, // force use of https for login/gatekeeper
-	cookie: 'ininauth',
-	cookie_options: {},
-	cookie_secret: null
+    secure: false, // force use of https for login/gatekeeper
+    cookie: 'ininauth',
+    cookie_options: {},
+    cookie_secret: null
 };
 
 class Inin {
-	constructor(options = {}) {
-		options = _.defaultsDeep(defaults, options);
-	}
+    constructor(options = {}) {
+        options = _.defaultsDeep(defaults, options);
+    }
 }
 
 Inin.VERSION = VERSION;
