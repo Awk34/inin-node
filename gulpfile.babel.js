b59bcc43bfb37e4338fcef9c123c58269ba0519f
Andrew Koroluk <koroluka@gmail.com>
2015-09-15 12:35:29 -0400

    feat(gulp): add inital gulp tasks: babel, watch, test

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,23 @@
 'use strict';
+import gulp from 'gulp';
+import babel from 'gulp-babel';
+import mocha from 'gulp-mocha';
 
-gulp.task('default', () => {
-    console.log('Hello World');
+gulp.task('babel', function() {
+    return gulp.src('src/*.js')
+        .pipe(babel())
+        .pipe(gulp.dest('dist'));
 });
+
+gulp.task('watch', function() {
+    gulp.watch('src/*.js', ['babel']);
+});
+
+gulp.task('test', ['babel'], function() {
+    return gulp.src(['test/*.js'])
+        .pipe(mocha({ reporter: 'spec' }))
+        .on('error', err => console.log(err.stack));
+});
+
+// Default Task
+gulp.task('default', ['babel', 'test']);
