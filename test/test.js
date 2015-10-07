4f095ac01df248229458d16d855d8670e6330e79
Andrew Koroluk <koroluka@gmail.com>
2015-10-07 18:19:59 -0400

    style(test):

--- test/test.js
+++ test/test.js
@@ -16,22 +16,14 @@ const request = bluebird.promisify(_request);
 const TEST_USERNAME = process.env.TEST_USERNAME || 'testhacker@example.com';
 const TEST_PASSWORD = process.env.TEST_PASSWORD || 'test1234';
 
-//inin.login(TEST_USERNAME, TEST_PASSWORD)
-//    .then(() => {
-//        //console.log(inin.configure());
-//        inin.getThing()
-//            .then(([inc, body]) => {
-//                console.log(body);
-//            });
-//    })
-//    .catch(::console.log);
+//inin.login(TEST_USERNAME, TEST_PASSWORD).then(() => {
+//    inin.getThing().then(([inc, body]) => {
+//        console.log(body);
+//    });
+//}).catch(console.log.bind(console));
 
 describe('inin', function() {
     describe('#login()', function() {
-        //before(function() {
-        //    return inin.login(TEST_USERNAME, TEST_PASSWORD);
-        //});
-
         it('should log in successfully', function() {
             return inin.login(TEST_USERNAME, TEST_PASSWORD).should.eventually.be.fulfilled;
         });
