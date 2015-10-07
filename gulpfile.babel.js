4f516a769f3969f5eccda89e4aaae71ae00f86e4
Andrew Koroluk <koroluka@gmail.com>
2015-10-07 14:15:17 -0400

    feat(gulp): add 'build' task, which runs 'babel' and new 'copy:dist' task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,17 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
 import mocha from 'gulp-mocha';
+import runSequence from 'run-sequence';
+import copy from 'gulp-copy';
+
+gulp.task('build', cb => {
+    runSequence(['babel', 'copy:dist'], cb);
+});
+
+gulp.task('copy:dist', function() {
+    gulp.src(['src/**/*', '!src/**/*.js'])
+        .pipe(gulp.dest('dist'));
+});
 
 gulp.task('babel', function() {
     return gulp.src('src/*.js')
@@ -13,11 +24,11 @@ gulp.task('watch', function() {
     gulp.watch('src/*.js', ['babel']);
 });
 
-gulp.task('test', ['babel'], function() {
+gulp.task('test', ['build'], function() {
     return gulp.src(['test/*.js'])
         .pipe(mocha({ reporter: 'spec' }))
         .on('error', err => console.log(err.stack));
 });
 
 // Default Task
-gulp.task('default', ['babel', 'test']);
+gulp.task('default', ['build', 'test']);
